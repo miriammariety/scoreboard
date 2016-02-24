3fcdff4ea9dd02d0946e5e93a75b62314aa6fdc5
Eli Tan <eliedrian1996@gmail.com>
2016-02-25 01:24:12 +0800

    create new models

--- live/admin.py
+++ live/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Event, Cluster, Scores
-# Register your models here.
-admin.site.register(Event)
+from live.models import Cluster, Event, Score
+
+
 admin.site.register(Cluster)
-admin.site.register(Scores)
+admin.site.register(Event)
+admin.site.register(Score)
