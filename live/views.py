8029a5834ba6ba35f94dfd1e96ad47ebb22e7b77
Miriam Marie Ty <mcty@up.edu.ph>
2016-02-27 22:35:47 +0800

    Comment out get context data

--- live/views.py
+++ live/views.py
@@ -6,15 +6,14 @@ from live.models import Event
 class HomeView(TemplateView):
     template_name = 'live/homepage.html'
 
-    def get_context_data(self, **kwargs):
-        context = super(HomeView, self).get_context_data(**kwargs)
-        context['upcoming_events'] = Event.objects.get_upcoming()
-        context['ongoing_events'] = Event.objects.get_current()
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super(HomeView, self).get_context_data(**kwargs)
+    #     context['upcoming_events'] = Event.objects.get_upcoming()
+    #     context['ongoing_events'] = Event.objects.get_current()
+    #     return context
 
 
 class ScheduleView(ListView):
     template_name = 'live/schedule.html'
     model = Event
     context_object_name = 'event_list'
-
