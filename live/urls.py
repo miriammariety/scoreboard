1c0daf61f313c161bf8adbd3fbfe73ba400ac467
Eli Edrian Tan <eliedrian1996@gmail.com>
2016-02-28 16:48:38 +0000

    checkout master url.py

--- live/urls.py
+++ live/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', views.HomeView.as_view(), name='home'),
+    url(r'home/$', views.HomeView.as_view(), name='home'),
     url(r'schedule/$', views.ScheduleView.as_view(), name='schedule'),
     url(r'scoreboard/$', views.ScoreboardView.as_view(), name='scoreboard'),
-    url(r'^cluster/(?P<pk>\w+)$', views.ClusterPageView.as_view(), name='cluster'),
+    url(r'cluster/(?P<pk>[\w-]+)$', views.ClusterPageView.as_view(), name='cluster'),
 ]
 
