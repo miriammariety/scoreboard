60e1b4bc17663e93ad0b9196338f0ecd5fac4fd1
Paks <jnpakson@gmail.com>
2016-02-28 23:58:32 +0800

    wawza

--- live/urls.py
+++ live/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'home/$', views.HomeView.as_view(), name='home'),
     url(r'schedule/$', views.ScheduleView.as_view(), name='schedule'),
+    url(r'scoreboard/$', views.ScoreboardView.as_view(), name='scoreboard'),
     url(r'^cluster/(?P<pk>\w+)$', views.ClusterPageView.as_view(), name='cluster'),
 ]
 
