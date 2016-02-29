2aab1e10beb50f3248282db2e0fd83c2cd0666df
Paks <jnpakson@gmail.com>
2016-02-29 18:10:11 +0800

    removed /home, remade homepage, schedule bugfix

--- live/urls.py
+++ live/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'home/$', views.HomeView.as_view(), name='home'),
+    url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'schedule/$', views.ScheduleView.as_view(), name='schedule'),
     url(r'scoreboard/$', views.ScoreboardView.as_view(), name='scoreboard'),
     url(r'cluster/(?P<pk>[\w-]+)$', views.ClusterPageView.as_view(), name='cluster'),
